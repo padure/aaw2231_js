e108364e1fc5b38d8b0b0af6b155e1b0cc317dff
padure <paduregheorghe7@gmail.com>
2025-01-14 14:51:03 +0200

    Laborator 1

---
+++ laborator1/index.js
@@ -0,0 +1,53 @@
+console.log("Eu invat JS");
+console.error("Eu am o eroare!");
+// Aici este un comentariu
+
+// Variabile
+var numeUser = 'John';
+let prenumeUser = 'Doe';
+let age = 38;
+
+console.log(numeUser, prenumeUser, age);
+console.log(numeUser +' ' + prenumeUser + ' ' + age);
+console.log(`Nume: ${numeUser}, Prenume: ${prenumeUser}, Varsta: ${age} ani`);
+
+
+console.log(city);
+var city = "Cahul";
+
+// Tipuri de date
+let salary = 25000;
+let name = "Alin Stejarel";
+let condition = false;
+let oras = {
+    'name': 'Cahul',
+    'population': 320000
+}
+let colors = ['red', 'green', 'blue'];
+let sum = function(){
+    return 5;
+}
+let data = null;
+console.log(data);
+
+// Constante
+const PORT = 8080;
+const IP_ADDRESS = "192.168.58.248";
+
+const suma = (a, b) => a + b;
+
+const users = [
+    'Mildred Kunze II',
+    'Miss Daryl Kuhlman',
+    'Nellie Cremin',
+    'Mr. Constance Toy',
+    'Alexander Rolfson',
+    'Manuel Koepp',
+    'Lionel Mayer',
+    'Patricia Spinka III',
+    'Lois Murphy Jr.',
+    'Annette Thompson',
+    'Mrs. Claude Beier',
+    'Roman Kling',
+    'Evelyn Zemlak',
+];
