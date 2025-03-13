3bd9938889da81a82b6430fdb600d238a8330a51
padure <paduregheorghe7@gmail.com>
2025-03-13 12:59:53 +0200

    Laborator 11

--- grupa1/laborator11/js/app.js
+++ grupa1/laborator11/js/app.js
@@ -8,16 +8,24 @@ const deleteTask = (e) => {
 const editTask = (e) => {
     const [task, ] = e.parentNode.parentNode.children;
     const newTask = prompt(`Modifica sarcina`, `${task.textContent}`);
-    task.textContent = newTask;
+    if(newTask != "" && newTask != null){
+        task.textContent = newTask;
+    } else{
+        alert('Introdu valori valide!');
+    }
 }
 //Functionalitate button Adauga
 adauga.addEventListener('click', () => {
     const task = prompt('Scrie o sarcina!');
-    tasks.innerHTML += `<li>
+    if(task != "" && task != null){
+        tasks.innerHTML += `<li>
         <div>${task}</div>
         <div>
             <button class='delete' onclick='deleteTask(this)'>Delete</button>
             <button class='edit' onclick='editTask(this)'>Edit</button>
         </div>
     </li>`;
+    } else{
+        alert('Introdu valori valide!');
+    }
 });
