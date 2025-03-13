053ffbc1edfd4c0552b8b306aee2212d20d0e6bf
padure <paduregheorghe7@gmail.com>
2025-03-13 13:13:34 +0200

    Laborator 11

--- grupa1/laborator11/js/app.js
+++ grupa1/laborator11/js/app.js
@@ -1,9 +1,27 @@
 const adauga = document.getElementById('adauga');
 const tasks = document.getElementById('tasks');
+const tasksData =  JSON.parse(localStorage.getItem('tasks')) ?? [];
 //Functions
 const deleteTask = (e) => {
-    if(confirm('Esti sigur?'))
+    if(confirm('Esti sigur?')){
+        const [taskText, ] = e.parentNode.parentNode.children;
         e.parentNode.parentNode.remove();
+        const newTasksList = tasksData.filter( task => task != taskText);
+        
+        localStorage.setItem('tasks', JSON.stringify(newTasksList));
+        displayTasks(newTasksList);
+    }
+}
+const displayTasks = (tasksList) => {
+    tasksList.forEach(task => {
+        tasks.innerHTML += `<li>
+            <div>${task}</div>
+            <div>
+                <button class='delete' onclick='deleteTask(this)'>Delete</button>
+                <button class='edit' onclick='editTask(this)'>Edit</button>
+            </div>
+        </li>`;
+    });
 }
 const editTask = (e) => {
     const [task, ] = e.parentNode.parentNode.children;
@@ -17,6 +35,8 @@ const editTask = (e) => {
 //Functionalitate button Adauga
 adauga.addEventListener('click', () => {
     const task = prompt('Scrie o sarcina!');
+    tasksData.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasksData));
     if(task != "" && task != null){
         tasks.innerHTML += `<li>
         <div>${task}</div>
@@ -28,4 +48,14 @@ adauga.addEventListener('click', () => {
     } else{
         alert('Introdu valori valide!');
     }
+});
+//Main
+tasksData.forEach(task => {
+    tasks.innerHTML += `<li>
+        <div>${task}</div>
+        <div>
+            <button class='delete' onclick='deleteTask(this)'>Delete</button>
+            <button class='edit' onclick='editTask(this)'>Edit</button>
+        </div>
+    </li>`;
 });
