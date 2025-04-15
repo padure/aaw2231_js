59d5248a380858fe4e6d3001b9118cb6cc5362b7
padure <paduregheorghe7@gmail.com>
2025-04-15 14:13:20 +0300

    Tema 14

--- tema14/js/app.js
+++ tema14/js/app.js
@@ -6,9 +6,33 @@ const buttonSubmit = inregistrareForm.querySelector('button[type="submit"]');
 acordCheck.addEventListener('change', (e) => {
     if(e.target.checked){
         buttonSubmit.removeAttribute('disabled');
+        buttonSubmit.classList.remove('disabled');
     } else{
         buttonSubmit.setAttribute('disabled', 'true');
+        buttonSubmit.classList.add('disabled');
     }
 });
 
-
+//Save data
+inregistrareForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = e.target.nume.value;
+    e.target.nume.nextElementSibling.textContent = 
+                        name === "" 
+                            ? "Completeaza campul nume"
+                            : "";
+    const data_nasterii = e.target.data_nasterii.value;
+    e.target.data_nasterii.nextElementSibling.textContent = 
+                        data_nasterii === "" 
+                            ? "Completeaza campul data nasterii"
+                            : "";
+    const localitate = e.target.localitate.value;
+    const genul = e.target.genul.value;
+    const [,last] = e.target.genul;
+    last.parentNode.nextElementSibling.textContent = 
+                genul === "" 
+                    ? "Completeaza campul genul"
+                    : "";
+    console.log(name, data_nasterii, localitate, genul);
+    inregistrareForm.reset();
+});
